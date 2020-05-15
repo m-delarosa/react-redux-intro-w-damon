fc1f85e9d7820df992547d4dec97cb604223b5f3
Michael De La Rosa <56710845+m-delarosa@users.noreply.github.com>
2020-05-15 09:27:58 -0600

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+import { createStore, combineReducers } from 'redux'
+import { Provider } from 'react-redux'
+
+const reducer = combineReducers({ todos })
+const store = createStore(reducer)
+
+console.log(store.getState())
+
+function todos(state = [], action) {
+  switch (action.type) {
+    case "ADD_TODO":
+      return [...state, action.payload]
+    default:
+      return state
+  }
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
